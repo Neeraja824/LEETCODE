691e94fc32e50eb128e04f18329f7b6620bdcf8d
Neeraja824 <155799322+Neeraja824@users.noreply.github.com>
2025-10-11 19:12:47 +0530

    Time: 0 ms (100%), Space: 43.2 MB (57.7%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n=nums.length,pivot=-1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                pivot=i-1;
+                break;
+            }
+        }
+        if(pivot!=-1){
+            for(int i=n-1;i>pivot;i--){
+                if(nums[pivot]<nums[i]){
+                    int temp=nums[i];
+                    nums[i]=nums[pivot];
+                    nums[pivot]=temp;
+                    break;
+                }
+            }
+        }
+        int l=pivot+1,r=n-1;
+        while(l<r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
